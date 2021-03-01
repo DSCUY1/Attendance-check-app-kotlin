02afcd99f0dcd1b16d0f9bb71bfa9d58307d1f37
Abdel Mfossa <abdelemfossa@gmail.com>
2021-03-01 13:03:19 +0100

    Begining of the login interface feature

---
+++ app/src/main/java/cm/uninet/attendancecheck/activities/LoginActivity.kt
@@ -0,0 +1,26 @@
+package cm.uninet.attendancecheck.activities
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import cm.uninet.attendancecheck.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Activation du plein ecran sur le splash screen
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+}
