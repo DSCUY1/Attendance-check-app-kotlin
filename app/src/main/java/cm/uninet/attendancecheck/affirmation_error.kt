3ba775f744f8f600e3e8a971ee7b80fe60589324
Nanfack Steve Ulrich <nanfacksteve7@gmail.com>
2021-03-04 12:46:09 +0100

    add affirmations OK and Error

---
+++ app/src/main/java/cm/uninet/attendancecheck/affirmation_error.kt
@@ -0,0 +1,11 @@
+package cm.uninet.attendancecheck
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class affirmation_error : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_affirmation_error)
+    }
+}
---
+++ app/src/main/java/cm/uninet/attendancecheck/affirmation_ok.kt
@@ -0,0 +1,11 @@
+package cm.uninet.attendancecheck
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class affirmation_ok : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_affirmation_ok)
+    }
+}
