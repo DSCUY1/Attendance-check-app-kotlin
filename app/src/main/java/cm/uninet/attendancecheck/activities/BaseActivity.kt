266f80e8e5122e8f1a6c9af80255c9b4dd9a1a54
Abdel Mfossa <abdelemfossa@gmail.com>
2021-03-04 11:27:09 +0100

    Finalisation de la Vue login avec simulation d'un compte admin

--- app/src/main/java/cm/uninet/attendancecheck/activities/BaseActivity.kt
+++ app/src/main/java/cm/uninet/attendancecheck/activities/BaseActivity.kt
@@ -1,12 +1,18 @@
 package cm.uninet.attendancecheck.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import cm.uninet.attendancecheck.R
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.dialog_progress.*
 
 open class BaseActivity : AppCompatActivity() {
+
+    private lateinit var mProgressDialog: Dialog
+    private lateinit var mDialog: Dialog
 
     fun showErrorSnackBar(message: String, errorMessage: Boolean){
         val snackBar =
@@ -17,17 +23,46 @@ open class BaseActivity : AppCompatActivity() {
             snackBarView.setBackgroundColor(
                 ContextCompat.getColor(
                     this@BaseActivity,
-                    R.color.secondaryColor
+                    R.color.bgSnackBarError
                 )
             )
         }else{
             snackBarView.setBackgroundColor(
                 ContextCompat.getColor(
                     this@BaseActivity,
-                    R.color.primaryColor
+                    R.color.bgSnackBarSuccess
                 )
             )
         }
         snackBar.show()
+    }
+
+    fun showProgressDialog(text: String){
+        mProgressDialog = Dialog(this)
+
+        mProgressDialog.setContentView(R.layout.dialog_progress)
+
+        mProgressDialog.tv_progress_text.text = text
+
+        mProgressDialog.setCancelable(false)
+        mProgressDialog.setCanceledOnTouchOutside(false)
+
+        mProgressDialog.show()
+    }
+    fun showDialog(text: String){
+        mDialog = Dialog(this)
+
+        mDialog.setContentView(R.layout.dialog_progress)
+
+        mDialog.tv_progress_text.text = text
+
+        mDialog.setCancelable(true)
+        mDialog.setCanceledOnTouchOutside(true)
+
+        mDialog.show()
+    }
+
+    fun hideProgressDialog(){
+        mProgressDialog.dismiss()
     }
 }
--- app/src/main/java/cm/uninet/attendancecheck/activities/LoginActivity.kt
+++ app/src/main/java/cm/uninet/attendancecheck/activities/LoginActivity.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import cm.uninet.attendancecheck.R
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : BaseActivity() {
+class LoginActivity : BaseActivity(), View.OnClickListener {
+//class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -25,12 +29,74 @@ class LoginActivity : BaseActivity() {
             )
         }
 
-        btn_login.setOnClickListener(){
-//          startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-            val intent = Intent(this@LoginActivity, MainActivity::class.java)
-            intent.putExtra("key", "Kotlin")
-            startActivity(intent)
-            finish()
+//        btn_login.setOnClickListener(){
+////          startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+//            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+//            intent.putExtra("key", "Kotlin")
+//            startActivity(intent)
+//            finish()
+//        }
+        btn_login.setOnClickListener(this)
+        forgot.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        if (view != null) {
+            when (view.id) {
+                R.id.forgot -> {
+                    showDialog("Veuillez contacter l'administrateur")
+                }
+                R.id.btn_login -> {
+                    loginUser()
+                }
+            }
+        }
+    }
+
+    private fun validateLoginDetails(): Boolean{
+        return when{
+            TextUtils.isEmpty(username.text.toString().trim { it <= ' '}) -> {
+                showErrorSnackBar("Veuillez entrer un nom d'utilisateur", true)
+                false
+            }
+            TextUtils.isEmpty(password.text.toString()) -> {
+                showErrorSnackBar("Veuillez entrer un mot de passe", true)
+                false
+            }
+            else -> {
+                showErrorSnackBar("Your details are valid.", false)
+                true
+            }
+        }
+    }
+
+    private fun loginUser(){
+        if (validateLoginDetails()){
+            showProgressDialog("Please wait...")
+
+            val user = username.text.toString().trim { it <= ' '}
+            val pass = password.text.toString()
+
+            if (user == "admin" && pass == "admin"){
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                intent.putExtra("username", user)
+                startActivity(intent)
+                finish()
+
+            } else {
+                if (user != "admin"){
+                    hideProgressDialog()
+                    showErrorSnackBar("Username incorrect !!!", true)
+                    t_username.endIconMode = TextInputLayout.END_ICON_NONE
+                    username.error = "Username incorrect !!!"
+                }
+                if (pass != "admin"){
+                    hideProgressDialog()
+                    showErrorSnackBar("Password incorrect !!!", true)
+                    t_password.endIconMode = TextInputLayout.END_ICON_NONE
+                    password.error = "Password incorrect !!!"
+                }
+            }
         }
     }
 }
